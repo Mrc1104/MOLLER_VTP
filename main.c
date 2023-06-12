27874830cee16aecc61d292586a26e63e15f8674
Matthew Ryan Conaway <mc321015@ohio.edu>
2023-06-12 17:02:09 -0400

    First Steps

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "moller_hls.h"
+
+int main(int argc, char *argv[])
+{
+	
+	printf("hello world");
+
+
+
+
+
+	return 1;
+}
---
+++ moller_hls.h
@@ -0,0 +1,10 @@
+#ifndef MOLLER_HLS_H
+#define MOLLER_HLS_H
+
+#include <include/ap_int.h>
+#include <include/hls_stream.h>
+
+
+
+
+#endif
