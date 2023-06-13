9489beae50f899c183f17c0dcfeb545cb07e1423
Ryan Conaway <mc321015@ohio.edu>
2023-06-13 15:16:45 -0400

    changed structure formats to obtain what we want  to from the fadc input streams

--- moller_hls.h
+++ moller_hls.h
@@ -24,41 +24,62 @@ typedef struct
 // - (VXS switch A and B are at VME slot positions 11,12 so the FADC cannot be installed here
 // - vxs_ch[ 144 ] to vxs_ch[ 159 ]: VME slot 5, ch 0 to 15 FADC channels
 // ...
-// - vxs_ch[ 208 ] to vxs_ch[ 224 ]: VME slot 19, ch 0 to 15 FADC channels // TODO: this should only go to 224 TOTAL channels
+// - vxs_ch[ 208 ] to vxs_ch[ 224 ]: VME slot 19, ch 0 to 15 FADC channels
 //
-#define N_CHAN_SEC 5; // number of FADC channels per sector // TODO: not sure what this val should be at the moment
+//#define N_CHAN_SEC 64; // number of FADC channels per sector (16 FADC ch per segment, 4 segment per sector)
+#define N_CHAN_SEC 224; // number of FADC channels per sector (16 FADC ch per segment, 4 segment per sector)
+// Be careful, max array n can make is 4096 bits wide (4096/16 = 256 so we are good)
 typedef struct
 {
 	hit_t vxs_chan[N_CHAN_SEC];
 } fadc_hits_t;
 
-// ring_t:
-// - e: energy measured in a ring (summed over all 28 segements)
-// - t: ring time, timestamped from ring central hit time appended to coarse 32ns frame counter
-// - nhits: number of hit channels in ring
+
+// ring_hit_t:
+// - Hit data for a single ring
+// - e: sum of the energy of all the hits on a ind. ring
+// - t: ring time measured at the central hit time
+// - nhits: number of hits on an ind. ring
+// - bbuff: nit buffer to make struct multiple of 8bits
 typedef struct
 {
 	ap_uint<16> e;
 	ap_uint<3> t;
-	ap_uint<5> nhits;
-} ring_t;
+	ap_uint<3> nhits;
+	ap_uint<2> bbuff;
+} ring_hit_t;
+
+// ring_loc_t:
+// - This is the general data structure to store the information about hits on a ring;
+//   which segment / sector the hit is in and the timing.
+// - trig: [0]=>0ns, [1]=>4ns, [2]=>8ns, ..., [7]=28ns, when bit=0 no trigger, when bit=1 trigger
+// - segment: bit map for the four segments per sector. bit=0, no hit; bit=1, hit
+// - sector: bit map for the seven sectors. bit=0, no hit; bit = 1 hit
+// - bbuf: bit buffer to make structs multiples of 8bits
+typedef struct
+{
+	ap_uint<8> trig;
+	ap_uint<4> segment;
+	ap_uint<7> sector;
+	ap_uint<5> bbuf;
+} ring_loc_t;
 
 typedef struct
 {
-	ring_t aring[8]; // we have eight rings
-} rings_all_t;
+	ring_loc_t loc;
+	ring_hit_t hits;
+} ring_data_t;
 
-// bitreg_t:
-// - bit register 
-// - Two uses: a timing bit register and a ring bit register
-// - Timing register: [0]=>0ns, [2]=>8ns, ..., [7]=>28ns
-// - Ring resiter: bits [0] - [7] correspond to the 8 rings. If [0] = 1 => ring one as a hit
-// - If [0] = 0 => ring one was not hit. etc.
-// - Note: I need one for each ring for the timing and the ring registers so its an array of these// - 8-bit registers
+// rings_all_t:
+// - An array to store the all the information for all 8 rings
+// - [0] = r1, [1] = r2, [2] = r3, [3] = r4
+//   [4] = r5a, [5] = r5b, [6] = r5c, [7] = r6
+//   bit = 0, no hit; bit = 1, hit
 typedef struct
 {
-	ap_uint<8> bitreg[8];
-} bitreg_t;
+	ring_data_t rings[8];
+} rings_all_t ;
+
 
 // moller_hls:
 // - main workhorse of the code where most of the logic is performed
@@ -75,8 +96,7 @@ void moller_hls
 	ap_uint<13> seed_threshold, // minimum energy for us to look at an individual hit
 	ap_uint<16> ring_threshold, // minimum summed energy (over one ring) to count a ring as hit
 	hls::stream<fadc_hits_t> &s_fadc_hits, // raw FADC data input stream
-	hls::stream<bitreg_t> rings, // stream for the bitmap of which rings were hit
-	hls::stream<bitreg_t> trigger // stream for the timing triggers
+	hls::stream<ring_data_t> rings // output stream for for the ring data
 );
 
 // define sub functions here
