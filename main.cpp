c0bc4c4ac312cd5e855d1698878ab30be1701780
Ryan Conaway <mc321015@ohio.edu>
2023-06-14 13:54:37 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,9 +8,9 @@
 
 int main(int argc, char *argv[])
 {
-	ap_uint<3> hit_det = 2; // time tolerance for coincidence (in 4ns ticks)
+	ap_uint<3> hit_dt = 2; // time tolerance for coincidence (in 4ns ticks)
 	ap_uint<13> seed_threshold = 5e3;
-	ap_uint<16> cluster_threshold = 10e3;
+	ap_uint<16> ring_threshold = 10e3;
 	
 	// hls::stream<T> behaves like a fifo array with infinite depth
 	// Once data has been read, it no longer is stored in any buffer
@@ -49,14 +49,16 @@ int main(int argc, char *argv[])
 	s_fadc_hits.write(fadc_hits);
 
 	while(!s_fadc_hits.empty()){
-		moller_hls( 
-			hit_det, 
-			seed_threshold, 
-			cluster_threshold, 
-			s_fadc_hits, 
-			s_trigger,
-			s_ring_trigger,
-			s_ring_all_t ); 
+	moller_hls
+	(
+		hit_dt,
+		seed_threshold, 
+		ring_threshold, 
+		s_fadc_hits, 
+		s_trigger, 
+		s_ring_trigger, 
+	 	s_ring_all_t 
+	);
 	}
 
 
