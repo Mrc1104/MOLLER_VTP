fd60039641f7721e0619bc82f117c07c5d5d7b24
Ryan Conaway <mc321015@ohio.edu>
2023-06-14 14:55:57 -0400

    uncommented out initlization loop

--- main.cpp
+++ main.cpp
@@ -25,10 +25,10 @@ int main(int argc, char *argv[])
 	testData.open("test_data/fake_FADC_data.txt");
 
 	fadc_hits_t fadc_hits;
-//	for(int ch = 0; ch < N_CHAN_SEC; ch++){
-//		fadc_hits.vxs_chan[ch].e;
-//		fadc_hits.vxs_chan[ch].t;
-//	}
+	for(int ch = 0; ch < N_CHAN_SEC; ch++){
+		fadc_hits.vxs_chan[ch].e;
+		fadc_hits.vxs_chan[ch].t;
+	}
 	do{
 		int index;
 		ap_uint<13> en;
