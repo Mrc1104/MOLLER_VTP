537eadd60cb744b844fc57a079fb9039f05794b8
Ryan Conaway <mc321015@ohio.edu>
2023-06-22 12:56:28 -0400

    sadness

--- main.cpp
+++ main.cpp
@@ -89,8 +89,8 @@ int main(int argc, char *argv[])
 	std::ifstream fchan_map;
 	fchan_map.open("chan_map/det_map_naive.conf");
 	/* If the chmap[][] is already created and stored in a header file, include it and comment out the I/O section*/
-	#include "chan_map/array1.h"
-	/*
+	// #include "chan_map/array1.h"
+	
 	chan_map chmap[N_SLOT][16]; // N_SLOT is defined in moller_hls.h
 								// Each FADC has 16 chans (one FADC per slot)
 	char tmp;
@@ -122,7 +122,7 @@ int main(int argc, char *argv[])
 	// Uncomment if you want to save the chan_map array to a file       //
 	// Remember to specify which name and path you want to save it in   //
 	save_chan_map_array(chmap,"chan_map/array1.h" );
-	*/
+	
 
 	
 	// load test data
