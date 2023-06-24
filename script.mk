ae686c2ccb310949513d9280e0901b99365c3c12
Matthew Ryan Conaway <mc321015@ohio.edu>
2023-06-24 16:54:01 -0400

    Added two .cpp files and set up makefile

--- script.mk
+++ script.mk
@@ -1,11 +1,17 @@
 OPTION:=
 PFILE:=
 HFILE:=
-configure:
+configure: configure.o
 	echo "Hello from makefile, target: configure"
 	echo $(OPTION)
 	echo $(PFILE)
-header:
+	./configure $(OPTION) $(PFILE)
+header: parser.o
 	echo "hello from makefile, target: header"
 	echo $(PILE)
 	echo $(HILE)
+	./parser $(PFILE) $(HFILE)
+configure.o: configure.cpp
+	g++ -o configure configure.cpp
+parser.o: parser.cpp
+	g++ -o parser parser.cpp
