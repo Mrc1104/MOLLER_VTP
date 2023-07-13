6e1effc94f51b915cf4338893158f2d7e1c9f629
Ryan Conaway <mc321015@ohio.edu>
2023-07-13 14:26:02 -0400

    created tb.cpp; added ofstream ofout; changed the majority of cout statements to fout.

---
+++ tb.cpp
@@ -0,0 +1,169 @@
+// output_check  
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+using std::cout; using std::endl;
+
+// for testing
+#include <fstream>
+#include <string>
+
+#include "moller_hls.h"
+
+// channel mapping
+#include "detector_type.h"
+#include "chan_map.h"
+#include <map>
+#include "chan_map/array1.h"
+
+// Fake data arrays
+#include "data_stream.h"
+
+void generateRndData(hit_t* vxs_chan){
+
+	std::srand(1); // set specific seed for testing latency
+	ap_uint<13> energy;
+	ap_uint<3> time;
+	for(int ch = 0; ch < N_CHAN; ch++){
+		energy = 0;
+		time = 0;
+		if(ch%8 < 4){ // rings 1 - 4
+           	if( (rand()%100) < 5 ){ // 5% chance
+               	energy = rand() % 8192;
+               	time = rand() % 8;
+           	}
+        }
+        else if( (ch%8 < 7) && (ch%8 > 3) ){ // rings 5a, 5b, 5c
+           	if( (rand()%100) < 10 ){ // 10% chance
+               	energy = rand() % 8192;
+               	time = rand() % 8;
+           	}
+        }
+        else if( ch%8 > 6){ // ring 6
+           	if( (rand()%100) < 5 ){ // 5% chance
+               	energy = rand() % 8192;
+               	time = rand() % 8;
+           	}
+        }
+		vxs_chan[ch].e = energy;
+		vxs_chan[ch].t = time;
+	}
+}
+
+
+int main(int argc, char *argv[])
+{
+	ap_uint<3> hit_dt = 2; // time tolerance for coincidence (in 4ns ticks)
+	// ap_uint<13> energy_threshold = 5e3;
+	ap_uint<13> energy_threshold = 1;
+	ap_uint<16> ring_threshold = 1;
+	
+	hls::stream<fadc_hits_t> s_fadc_hits; // raw data stream from the
+	hls::stream<trigger_t> s_time_trigger; // output stream for for the trigger data
+	hls::stream<ring_trigger_t> s_ring_trigger; // output stream for for the ring trigger data
+	hls::stream<ring_all_t> s_ring_all_t; // output stream for the ring data
+
+	char tmp;
+	int ch;
+	std::string detector_id;
+	int seg_num;
+	int sub_elem;
+
+	fadc_hits_t fadc_hits;
+	for(int ch = 0; ch < N_CHAN; ch++){
+		fadc_hits.vxs_chan[ch].e = 0;
+		fadc_hits.vxs_chan[ch].t = 0;
+	}
+
+	// generateRndData(fadc_hits.vxs_chan);
+	for(int i = 0 ; i <N_CHAN; i++){
+		fadc_hits.vxs_chan[i].e = fake_data_0[i].e;
+		fadc_hits.vxs_chan[i].t = fake_data_0[i].t;
+	}	
+	s_fadc_hits.write(fadc_hits);
+	for(int i = 0 ; i <N_CHAN; i++){
+		fadc_hits.vxs_chan[i].e = fake_data_1[i].e;
+		fadc_hits.vxs_chan[i].t = fake_data_1[i].t;
+	}	
+	s_fadc_hits.write(fadc_hits);
+	for(int i = 0 ; i <N_CHAN; i++){
+		fadc_hits.vxs_chan[i].e = fake_data_2[i].e;
+		fadc_hits.vxs_chan[i].t = fake_data_2[i].t;
+	}	
+	s_fadc_hits.write(fadc_hits);
+
+	while(!s_fadc_hits.empty()){
+
+		moller_hls
+		(
+			hit_dt,
+			energy_threshold,
+			ring_threshold,
+			chmap,
+			s_fadc_hits,
+			s_time_trigger,
+			s_ring_trigger,
+	 		s_ring_all_t
+		);
+
+	}
+
+	std::ofstream fout("tb_output.dat");
+	// TRIGGER INFO BLOCK
+    fout << "\nRing Data:__________________\n";
+	while(!s_ring_all_t.empty())
+	{
+		ring_all_t ring_data = s_ring_all_t.read();
+		for(int ring_index = 0; ring_index < 8; ring_index++){
+			fout << "ringNum: " << ring_index << endl;
+			fout << "ring_data.r[" << ring_index << "].e: " << ring_data.r[ring_index].e << endl;
+			fout << "ring_data.r[" << ring_index << "].nhits: " << ring_data.r[ring_index].nhits << endl;
+			// cout << "ring_data.r[" << i << "].segment: " << ring_data.r[i].segment << endl;
+			fout << "ring_data.r[" << ring_index <<"].segment: ";
+			for(int seg_index = 27; seg_index > -1; seg_index--){
+				fout << "[" << ring_data.r[ring_index].segment[seg_index] << "]";
+			}
+			fout << endl;
+		}
+	}
+
+
+    fout << "\nRing Trig Data:__________________\n";
+	printf("\nRing Trig Data:__________________\n");
+	fout << "Format:\t 7 ------------------ 0\t ring #" << endl;
+	while(!s_ring_trigger.empty())
+	{
+		ring_trigger_t ring_trig = s_ring_trigger.read();
+		for(int ring_index = 7; ring_index > -1; ring_index--){
+			fout << "[" << ring_trig.ring[ring_index] << "]";
+		}
+		fout << endl;
+	}
+	
+    fout << "\n\nTime Trig Data:__________________\n";
+	printf("\n\nTime Trig Data:__________________\n");
+	while(!s_time_trigger.empty())
+	{
+		int t32ns = 0;
+		trigger_t time_trig = s_time_trigger.read();
+
+		fout << "Format:\t 12ns ------------------ -16ns\t (4ns ticks)" << endl;
+		for(int ring_index = 0; ring_index < 8; ring_index ++){
+			for(int time_tick = 7; time_tick > -1; time_tick--){
+				if(time_trig.trig[ring_index][time_tick]){
+					printf("Trigger found at T=%dns\n", t32ns*32+time_tick*4-16);
+				}
+			}
+			fout << "Ring: " << ring_index << "\t";
+			for(int time_tick = 7; time_tick > -1; time_tick--){
+				fout << "[" << time_trig.trig[ring_index][time_tick] << "]";
+			}
+			fout << endl;
+		}
+		t32ns++;
+	}
+
+	
+	return 0;
+}
